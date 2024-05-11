e271aac24207f3e757f2da6db92482fd70c6d06e
anus-ahmed <148375820+anus-ahmed@users.noreply.github.com>
2024-05-12 01:03:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+setInterval(function(){
+    let bolt = document.getElementsByClassName('fa-bolt')[0];
+    if(bolt.classList.contains('bolt-green')){
+        bolt.classList.remove('bolt-green');
+        bolt.classList.add('bolt-red');
+    }
+    else if(bolt.classList.contains('bolt-red')){
+        bolt.classList.remove('bolt-red');
+        bolt.classList.add('bolt-green');
+    }
+}, 2000);
+
 function checkTheme() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.className += 'darkMode';
@@ -16,7 +28,7 @@ function changeTheme(){
     else{
         document.body.className = 'lightMode';
     }
-}
+};
 
 window.onload = checkTheme;
 document.getElementById('themeSwitcher').onclick = changeTheme;
