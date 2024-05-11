97bbaea2064b3f4c582dd586e3683ae1ca4732a3
anus-ahmed <148375820+anus-ahmed@users.noreply.github.com>
2024-05-11 23:09:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function checkTheme() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.className += 'darkMode';
+        document.getElementById('themeSwitcher').checked = true;
     }
     else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
         document.body.className += 'lightMode';
+        document.getElementById('themeSwitcher').checked = false;
+    }
+};
+
+function changeTheme(){
+    if(document.getElementById('themeSwitcher').checked){
+        document.body.className = 'darkMode';
+    }
+    else{
+        document.body.className = 'lightMode';
     }
 }
+
 window.onload = checkTheme;
+document.getElementById('themeSwitcher').onclick = changeTheme;
