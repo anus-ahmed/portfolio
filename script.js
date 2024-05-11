b0a80b6dd8f3b2bfb40754dd4b0cd1a03b65baa1
anus-ahmed <148375820+anus-ahmed@users.noreply.github.com>
2024-05-11 22:46:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function checkTheme() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('darkMode');
+    }
+    else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        document.body.classList.add('lightMode');
+    }
+}
 
+window.onload = checkTheme;
