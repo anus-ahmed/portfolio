79017ae280955f05318e093d08f8b8611ce20cdd
anus-ahmed <148375820+anus-ahmed@users.noreply.github.com>
2024-05-12 16:43:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ setInterval(function(){
 function checkTheme() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.className += 'darkMode';
-        document.getElementById('themeSwitcher').checked = true;
+        document.getElementById('checkbox').checked = true;
         document.getElementById('titleName').className += " gradient-heading";
     }
     else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
         document.body.className += 'lightMode';
-        document.getElementById('themeSwitcher').checked = false;
+        document.getElementById('checkbox').checked = false;
         document.getElementById('titleName').classList.remove("gradient-heading");
     }
 };
 
 function changeTheme(){
-    if(document.getElementById('themeSwitcher').checked){
+    if(document.getElementById('checkbox').checked){
         document.body.className = 'darkMode';
         document.getElementById('titleName').className += " gradient-heading";
     }
@@ -35,4 +35,4 @@ function changeTheme(){
 };
 
 window.onload = checkTheme;
-document.getElementById('themeSwitcher').onclick = changeTheme;
+document.getElementById('checkbox').onclick = changeTheme;
