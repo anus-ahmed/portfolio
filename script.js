e73e377d931da23fe11a4fe1b8234477e07ac5fc
anus-ahmed <148375820+anus-ahmed@users.noreply.github.com>
2024-05-19 21:31:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var uiContainer = document.getElementById('projects');
+
+const projects = [
+    {
+        "year": 2023,
+        "title": "UBL Retail Web Application",
+        "descr": "lorem ipsum dolor sit amet",
+        "urltitle": "google.com",
+        "url": "https://www.google.com/"
+    },
+    {
+        "year": 2022,
+        "title": "UBL Retail Web Application 2",
+        "descr": "lorem ipsum dolor sit amet 2",
+        "urltitle": "google.com",
+        "url": "https://www.google.com/"
+    }
+]
+projects.forEach(listProjects);
+
+function listProjects(item, index) {
+    uiContainer.innerHTML += "<div class='project-details'><div class='proj-title'><h3 class='font-barlow h3 fw-800' >" + item.title + "</h3><span class='font-shareTech p3 fw-400 badge'>" + item.year + "</span></div><p class='font-shareTech p3 fw-400'>" + item.descr + "</p><a href='" + item.url + "' class='font-shareTech p3 fw-500 link-colored' target='blank'>" + item.urltitle + "&nbsp;&nbsp;<i class='fa-solid fa-arrow-right'></i></a>";
+};
+
 function checkTheme() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.className += 'darkMode';
@@ -11,6 +35,8 @@ function checkTheme() {
     }
 };
 
+window.onload = checkTheme;
+
 function changeTheme() {
     if (document.getElementById('checkbox').checked) {
         document.body.className = 'darkMode';
@@ -22,5 +48,4 @@ function changeTheme() {
     }
 };
 
-window.onload = checkTheme;
 document.getElementById('checkbox').onclick = changeTheme;
